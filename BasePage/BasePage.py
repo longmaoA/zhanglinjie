c63411a2f74cbfef753306651c7832e17d14d1d3
xinzhen <xinzhen@huxiu.com>
2019-12-13 18:02:51 +0800

    优化 BasePage 代码优化

--- BasePage/BasePage.py
+++ BasePage/BasePage.py
@@ -22,25 +22,22 @@ def stop_appium_sever(post_num):
             p1 = int(p0.split('LISTENING')[1].strip()[0:4])  # 获取进程号
             os.popen(f'taskkill /F /PID {p1}')  # 结束进程
             print('appium server已结束')
-    elif _system_name == 'Darwin':
+    elif _system_name == 'MAC':
         p = os.popen(f'lsof -i tcp:{post_num}')
         p0 = p.read()
         if p0.strip() != '':
             p1 = int(p0.split('\n')[1].split()[1])  # 获取进程号
             os.popen(f'kill {p1}')  # 结束进程
-            print('appium server已结束!')
+            print('appium server已结束')
 
 
 def start_appium_sever(post_num):
-    """
-    开启appium服务
-    """
-    # 先判断端口是否被占用，如果被占用则关闭该端口号
-    stop_appium_sever(post_num)
+    """开启appium服务"""
+    stop_appium_sever(post_num)  # 先判断端口是否被占用，如果被占用则关闭该端口号
     # 根据系统，启动对应的服务
     cmd_dict = {
         'Windows': f' start /b appium -a 127.0.0.1 -p {post_num} --session-override --no-reset --local-timezone',
-        'Darwin': f'appium -a 127.0.0.1 -p {post_num} --session-override --no-reset --local-timezone &'
+        'MAC': f'appium -a 127.0.0.1 -p {post_num} --session-override --no-reset --local-timezone &'
     }
     os.system(cmd_dict[_system_name])
     time.sleep(3)  # 等待启动完成
@@ -49,6 +46,7 @@ def start_appium_sever(post_num):
 
 class AppiumDriver:
     driver: WebDriver = None
+
     @classmethod
     def app_driver(cls, port=4723):
 
