b870f3410a6a52ff41945853d6d118007986a0ee
xinzhen <xinzhen@huxiu.com>
2019-12-16 09:29:38 +0800

    独立出 Driver和Sever AppiumDriver 和 AppiumSever 独立处理，新增TestReports文件夹为git上传忽略文件夹

---
+++ BasePage/AppiumDriver.py
@@ -0,0 +1,41 @@
+
+from appium import webdriver
+from BasePage.AppiumSever import AppiumSever
+from selenium.webdriver.remote.webdriver import WebDriver
+
+
+class AppiumDriver:
+
+    driver: WebDriver = None
+
+    def __init__(self):
+        self.start_sever = AppiumSever().start_appium_sever
+
+    def app_driver(self, port=4725):
+        # 启动APPIUM sever，并给sever传递一个端口号
+        self.start_sever(port)
+
+        caps = {
+            'platformName': 'Android',
+            'deviceName': '诺基亚',
+            'appPackage': 'com.huxiu',
+            'appActivity': '.ui.activity.SplashActivity',
+            'automationName': 'uiautomator2',
+            'autoGrantPermissions': True,
+            'printPageSourceOnFindFailure': True,
+            'newCommandTimeout': '66',
+            'chromedriverExecutable': '/Users/PycharmProjects/System/Mobile/Mini/chromedriver_240',
+            'neReset': True,
+            # 'fullReset': False,
+            # 'unicodeKeyboard': True,
+            # 'resetKeyboard': True,
+            # 'chromeOptions': {
+            #     'androidProcess': 'com.tencent.mm:appbrand0'
+            #           }
+        }
+        driver = webdriver.Remote("http://localhost:" + str(port) + "/wd/hub", caps)
+        driver.implicitly_wait(10)  # 智能等待 10 秒
+        return driver
+
+    def quit_driver(self):
+        self.driver.quit()
---
+++ BasePage/AppiumSever.py
@@ -0,0 +1,42 @@
+import os
+import time
+import platform
+
+
+class AppiumSever:
+    # 获取当前系统
+    _system_name = platform.system()
+
+    def stop_appium_sever(self, post_num):
+        """
+        关闭appium服务
+        """
+        if self._system_name == 'Windows':
+            p = os.popen(f'netstat -aon | findstr {post_num}')
+            p0 = p.read().strip()
+            if p0 != '' and 'LISTENING' in p0:
+                p1 = int(p0.split('LISTENING')[1].strip()[0:4])  # 获取进程号
+                os.popen(f'taskkill /F /PID {p1}')  # 结束进程
+                print('\nAppium server 已结束\n')
+        elif self._system_name == 'MAC':
+            p = os.popen(f'lsof -i tcp:{post_num}')
+            p0 = p.read()
+            if p0.strip() != '':
+                p1 = int(p0.split('\n')[1].split()[1])  # 获取进程号
+                os.popen(f'kill {p1}')  # 结束进程
+                print('\nAppium server 已结束\n')
+
+    def start_appium_sever(self, post_num):
+        """
+        开启appium服务
+        """
+        # 先判断端口是否被占用，如果被占用则关闭该端口号
+        self.stop_appium_sever(post_num)
+        # 根据系统，启动对应的服务
+        cmd_dict = {
+            'Windows': f' start /b appium -a 127.0.0.1 -p {post_num} --session-override --no-reset --local-timezone',
+            'MAC': f'appium -a 127.0.0.1 -p {post_num} --session-override --no-reset --local-timezone &'
+        }
+        os.system(cmd_dict[self._system_name])
+        time.sleep(3)  # 等待启动完成
+        print('\n Appium sever 启动成功!\n')
